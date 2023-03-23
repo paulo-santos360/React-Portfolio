9ebdf4e41a36833292d97a68691337bcdd4964f0
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-03-23 06:25:49 -0300

    atualizar

--- portfolio/src/components/Sidebar.jsx
+++ portfolio/src/components/Sidebar.jsx
@@ -1,10 +1,15 @@
+import Avatar from '../img/foto-1.jpeg'
+
+import "../styles/components/sidebar.sass"
+
 const Sidebar = () => {
     return(
         <aside id="sidebar">
             <p>imagem</p>
             <p className="title">Desenvolvedor</p>
+            <p>redes sociais</p>
             <p>informações de contato</p>
-            <p href="" className="btn">Download</p>
+            <a href="" className="btn">Download</a>
         </aside>
     )
 }
