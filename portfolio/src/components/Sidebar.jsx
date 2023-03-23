ecd130818967b15a25858346fcb67daf669af851
Paulo Santos <102436341+pauloninja@users.noreply.github.com>
2023-03-23 17:19:38 -0300

    Atualizar

--- portfolio/src/components/Sidebar.jsx
+++ portfolio/src/components/Sidebar.jsx
@@ -1,13 +1,13 @@
-import Avatar from '../img/foto-1.jpeg'
 
-import "../styles/components/sidebar.sass"
+import Avatar from '../img/foto-1.jpeg';
+
+import "../styles/components/sidebar.sass";
 
 const Sidebar = () => {
     return(
         <aside id="sidebar">
             <img src={Avatar} alt="Paulo Santos"  />
             <p className="title">Desenvolvedor</p>
-            <p>redes sociais</p>
             <p>informações de contato</p>
             <a href="" className="btn">Download</a>
         </aside>
